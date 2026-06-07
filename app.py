c98f06a2ec0e1a0c6e2eee4c674a1e66420ab7b4
aarya-tanwade <buildwithaarya@gmail.com>
2026-06-07 18:04:02 +0530

    greet, add & factorial

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ def add(a, b):
 
 result = add(5, 3)
 print(f"The sum of 5 and 3 is: {result}") 
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+number = 5
+fact_result = factorial(number)
+print(f"The factorial of {number} is: {fact_result}")
